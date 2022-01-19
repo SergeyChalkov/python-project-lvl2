51b607f0f7caa89f9fea0d15731ab4ab1353cd9a
seregil <seregilr@gmail.com>
2022-01-19 23:07:24 +0700

    finish 1/9

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import argparse
+
 
 def main():
-    print('Hello!')
+    return parser.parse_args()
 
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
 
 if __name__ == '__main__':
-    main()
+    args = main()
