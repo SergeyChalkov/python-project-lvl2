030e30d86c9cb1cecc5e5aa8bc7185ea5f7ea2f9
seregil <seregilr@gmail.com>
2022-01-19 23:20:30 +0700

    2 / 9

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,6 +9,7 @@ def main():
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '--format', help='set format of output')
 
 if __name__ == '__main__':
     args = main()
