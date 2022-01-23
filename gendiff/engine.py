9a6abd5a297e380a1d72b435f09d0b9c984191c0
seregil <seregilr@gmail.com>
2022-01-23 23:47:07 +0700

    gendiff refactor

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -8,17 +8,21 @@ _equal = '  '
 
 def generate_diff(old_file: dict, new_file: dict) -> str:
     diff = []
-    for key, value in old_file.items():
-        new_value = new_file.pop(key, None)
-        if new_value:
-            if new_value == value:
-                diff.append((_equal + str(key), value))
+
+    def compare_old_values():
+        for key, value in old_file.items():
+            new_value = new_file.pop(key, None)
+            if new_value:
+                if new_value == value:
+                    diff.append((_equal + str(key), value))
+                else:
+                    diff.extend(
+                        [(_rem + str(key), value), (_add + str(key), new_value)]
+                    )
             else:
-                diff.extend(
-                    [(_rem + str(key), value), (_add + str(key), new_value)]
-                )
-        else:
-            diff.append((_rem + str(key), value))
+                diff.append((_rem + str(key), value))
+
+    compare_old_values()
     for key, value in new_file.items():
         diff.append((_add + str(key), value))
     diff.sort(key=lambda x: x[0][2:])
