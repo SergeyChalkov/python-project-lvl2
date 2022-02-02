439cbe2037ef82c7f9e762b4caaa85b635ca4c24
seregil <seregilr@gmail.com>
2022-02-03 00:26:23 +0700

    add tests for yaml format

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,22 @@
 import json
 import pytest
+import yaml
 from pathlib import Path
 
 
 fixtures_dir = Path('tests/fixtures')
 
 
-@pytest.fixture(scope='session')
-def flat_one():
+@pytest.fixture(params=[json.load, yaml.safe_load])
+def flat_one(request):
     with (fixtures_dir / 'first_flat.json').open() as file:
-        return json.load(file)
+        return request.param(file)
 
 
-@pytest.fixture(scope='session')
-def flat_two():
+@pytest.fixture(params=[json.load, yaml.safe_load])
+def flat_two(request):
     with (fixtures_dir / 'second_flat.json').open() as file:
-        return json.load(file)
+        return request.param(file)
 
 
 @pytest.fixture(scope='session')
